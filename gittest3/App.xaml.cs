e835839584e338610a1c4b09dd9c4a946aef3dfd
macUser <macUser@SPICEPC>
2024-01-04 16:21:19 +0900

    プロジェクト ファイルを追加します。

---
+++ gittest3/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace gittest3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
